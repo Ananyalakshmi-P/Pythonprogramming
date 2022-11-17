da633fcc3f0f4e568f9c70feb990a92b24d85b34
Ananyalakshmi-P <117572493+Ananyalakshmi-P@users.noreply.github.com>
2022-11-17 20:09:28 +0530

    Add files via upload

--- stopt watch.py
+++ stopt watch.py
@@ -1,8 +1,16 @@
 import time
 second=0
+minutes=0
+n=0
 a=int(input("enter the count"))
 while second<=a:
     print(second)
     time.sleep(1)
     second=second+1
+    n=n+1
+    if second==n*60:
+        minute=(second)/60
+        print(minute)
+
+
 
