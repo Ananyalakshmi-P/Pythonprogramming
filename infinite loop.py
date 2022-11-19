9cfad756ce9392d823f18eb00201bf5b62af9594
Ananyalakshmi-P <117572493+Ananyalakshmi-P@users.noreply.github.com>
2022-11-19 16:59:39 +0530

    Add files via upload

---
+++ infinite loop.py
@@ -0,0 +1,6 @@
+import random
+
+while True:
+    dice_value_obtained=random.randint(1,6)
+    print(dice_value_obtained)
+    key=input('press enter key to continue')
---
+++ secret word.py
@@ -0,0 +1,12 @@
+counter=0
+secret_word= "Ananyalp"
+while counter<7:
+    word=input("Enter the secreat word")
+
+    counter=counter+1
+
+    if word==secret_word:
+        print("you guessed it right")
+        break
+else:
+    print("Better luck next time")
